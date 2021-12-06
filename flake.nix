67174b8ac3dbd40558b21f0353f1f60fb8b7abef
David Arnold <david.arnold@iohk.io>
2021-12-06 16:46:07 -0500

    cleanup the flake
    - the new standard is to avoid too much "unconventional magic" (such as
      simpleFlake) at the top-level flake
    - the top level flake should read like a TOC even to an average nix
      nix flakes user

--- flake.nix
+++ flake.nix
@@ -2,10 +2,14 @@
   description = "Bitte for infra-ops";
 
   inputs = {
-    utils.url = "github:kreisys/flake-utils";
+    utils.url = "github:numtide/flake-utils";
+
+    # --- Bitte Stack ----------------------------------------------
     bitte.url = "github:input-output-hk/bitte";
-    nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
     nixpkgs.follows = "bitte/nixpkgs";
+    nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
+    # --------------------------------------------------------------
+
     bitte-ci.url = "github:input-output-hk/bitte-ci";
     bitte-ci.inputs.bitte.follows = "bitte";
     ops-lib.url = "github:input-output-hk/ops-lib/zfs-image?dir=zfs";
@@ -16,43 +20,76 @@
   };
 
   outputs = { self, nixpkgs, utils, bitte, ipxed, ... }@inputs:
-    utils.lib.simpleFlake {
-      inherit nixpkgs;
-      systems = [ "x86_64-linux" ];
 
-      preOverlays = [ bitte ipxed ];
-      overlay = import ./overlay.nix inputs;
+    (
+      let
+        overlays = [ pkgsOverlay auxOverlay bitte.overlay ipxed.overlay];
 
-      extraOutputs = let
+        pkgsOverlay = final: prev: {
+          inherit (inputs.nixpkgs-unstable.legacyPackages."${prev.system}")
+            hydra-unstable vector;
+          bitte-ci = inputs.bitte-ci.packages."${prev.system}";
+          inherit (inputs.nomad-driver-nix.packages."${prev.system}") nomad-driver-nix;
+          nomad-follower = inputs.nomad-follower.defaultPackage."${prev.system}";
+        };
+
+        auxOverlay = final: prev: {
+          jobs = let
+            src =
+              inputs.nix-inclusive.lib.inclusive ./. [ ./cue.mod ./deploy.cue ./jobs ];
+
+            exported = final.runCommand "defs" { buildInputs = [ final.cue ]; } ''
+              cd ${src}
+              cue export -e jobs -t sha=${inputs.self.rev or "dirty"} > $out
+            '';
+
+            original = builtins.fromJSON (builtins.readFile exported);
+
+            runner = name: value:
+              let jobFile = builtins.toFile "${name}.json" (builtins.toJSON value);
+              in final.writeShellScriptBin name ''
+                echo "Running job: ${jobFile}"
+                ${final.nomad}/bin/nomad job run ${jobFile}
+              '';
+          in prev.lib.mapAttrs runner original;
+        };
+
+        pkgsForSystem = system: import nixpkgs {
+          inherit overlays system;
+          config.allowUnfree = true;
+        };
+
         bitteStack = bitte.lib.mkBitteStack {
           inherit self inputs;
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            overlays =
-              [ bitte.overlay ipxed.overlay (import ./overlay.nix inputs) ];
-          };
-
+          pkgs = pkgsForSystem "x86_64-linux";
           domain = "infra.aws.iohkdev.io";
           clusters = ./clusters;
           deploySshKey = "./secrets/ssh-infra-production";
           hydrateModule = _: { };
         };
-      in { inherit self inputs; } // bitteStack;
 
-      # simpleFlake ignores devShell if we don't specify this.
-      packages = { jobs }@pkgs: pkgs;
+      in
+      utils.lib.eachSystem [ "x86_64-linux" ]
+        (system: rec {
 
-      hydraJobs = { ipxed }@pkgs: pkgs;
+          legacyPackages = pkgsForSystem system;
 
-      devShell = { bitteShell, cue }:
-        bitteShell {
-          inherit self;
-          cluster = "infra-production";
-          namespace = "default";
-          profile = "infra-ops";
-          region = "us-west-1";
-          domain = "infra.aws.iohkdev.io";
-          extraPackages = [ cue ];
-        };
-    };
+          devShell = legacyPackages.bitteShell rec {
+            inherit self;
+            cluster = "infra-production";
+            namespace = "default";
+            profile = "infra-ops";
+            region = "us-west-1";
+            domain = "infra.aws.iohkdev.io";
+            extraPackages = with legacyPackages; [ cue ];
+          };
+
+        }) // {
+        # eta reduce not possibe since flake check validates for "final" / "prev"
+        overlay = final: prev: nixpkgs.lib.composeManyExtensions overlays final prev;
+      } // bitteStack
+    )
+
+  ; # outputs
+
 }
