fcee86464765c17b7db490133a42b05fd7e94d17
Robin Stumm <robin.stumm@iohk.io>
2022-01-10 15:51:54 +0100

    remove mantis binary cache (no longer exists)

--- clusters/infra/production/default.nix
+++ clusters/infra/production/default.nix
@@ -164,13 +164,11 @@ in {
   nix.binaryCaches = [
     "https://hydra.iohk.io"
     "https://cache.nixos.org"
-    "https://hydra.mantis.ist"
   ];
 
   nix.binaryCachePublicKeys = [
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "hydra.mantis.ist-1:4LTe7Q+5pm8+HawKxvmn2Hx0E3NbkYjtf1oWv+eAmTo="
   ];
 
   cluster = {
